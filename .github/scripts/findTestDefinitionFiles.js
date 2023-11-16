432bd4fdec5acfa9ee32190a3bb1593a1fd86ccd
Mike Bruzina <mbruzina@newrelic.com>
2023-11-16 10:51:43 -0600

    hacking

---
+++ .github/scripts/findTestDefinitionFiles.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const fsp = fs.promises;
+const path = require("path");
+
+// readdir recursive directory search
+const {readdir} = fsp;
+
+async function getFiles(dir) {
+    const dirents = await readdir(dir, {withFileTypes: true});
+    const files = await Promise.all(
+        dirents.map((dirent) => {
+            const res = path.join(dir, dirent.name);
+            return dirent.isDirectory() ? getFiles(res) : res;
+        })
+    );
+    return Array.prototype.concat(...files);
+}
+
+
+async function main() {
+    const definitionsDir = "/Users/mbruzina/Projects/github/newrelic/open-install-library/test/definitions";
+    const testDefinitions = await getFiles(definitionsDir);
+
+    const outputTestFilesMap = testDefinitions
+        .map((testDefinitionFile) => {
+            return {
+                testDefinitionFile,
+                testDisplayName: testDefinitionFile.replace(`${definitionsDir}/`, ""),
+            };
+        });
+
+    const output = {
+        include: outputTestFilesMap,
+    };
+    console.log(output);
+    return output;
+}
+
+
+if (require.main === module) {
+    main();
+}
