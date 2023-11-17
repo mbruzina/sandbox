35c54a0b58e61f88b6b1422ed0cbebb38c85f366
Mike Bruzina <mbruzina@newrelic.com>
2023-11-17 11:29:42 -0600

    hacking

--- .github/scripts/findTestDefinitionFiles.js
+++ .github/scripts/findTestDefinitionFiles.js
@@ -19,8 +19,8 @@ async function getFiles(dir) {
 
 async function main() {
     // const definitionsDir = "${{ github.workspace }}/oil/test/definitions";
-    const definitionsDir = "/home/runner/work/sandbox/sandbox/oil/test/definitions/infra-agent";
-    const abbrevDir = "/test/definitions/infra-agent"
+    const definitionsDir = "/home/runner/work/sandbox/sandbox/oil/test/definitions";
+    const abbrevDir = "/test/definitions"
     const testDefinitions = await getFiles(definitionsDir);
 
     const outputTestFilesMap = testDefinitions
